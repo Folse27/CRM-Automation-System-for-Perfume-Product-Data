1a03dd044d7f0f01d9e68cca350b2250e89df76b
Folse27 <folse279@gmail.com>
2026-03-24 20:02:19 +0100

    product_data_enricher.py

--- product_data_enricher.py
+++ product_data_enricher.py
@@ -1065,6 +1065,7 @@ async def main_func(browser, product, price, sku, identifier, category_id, makeu
                     if hit_tokens == expected_tokens:
                         print("MATCHED WITH CONCENTRATION (EXACT)", flush=True)
                         url_field = hit.get("url")
+                        print("URL FIELD TYPE:", type(url_field), "VALUE:", url_field, flush=True)
                         if isinstance(url_field, dict):
                             print(url_field, flush=True)
                             if "UK" in url_field and url_field["UK"]:
@@ -1082,6 +1083,7 @@ async def main_func(browser, product, price, sku, identifier, category_id, makeu
                     if hit_tokens == tokens:
                         print("MATCHED TOKENS ONLY (EXACT)", flush=True)
                         url_field = hit.get("url")
+                        print("URL FIELD TYPE:", type(url_field), "VALUE:", url_field, flush=True)
                         if isinstance(url_field, dict):
                             print(url_field, flush=True)
                             if "UK" in url_field and url_field["UK"]:
